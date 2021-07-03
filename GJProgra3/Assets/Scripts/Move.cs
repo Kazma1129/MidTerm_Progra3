f2167979db8c1adeb5054be722b20d22fe6f9fbb
Kazma <mariocastro1205@gmail.com>
2021-07-03 08:05:38 -0700

    Extra prefabs
    added extra prefabs to account for resolution

---
+++ GJProgra3/Assets/Scripts/Move.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Move : MonoBehaviour
+{
+    private float x;
+    private float y;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
